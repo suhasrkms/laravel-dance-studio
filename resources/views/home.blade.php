3f146c345e5a03430158b44652232db29ba61030
Suhas R <suhasrkms@gmail.com>
2020-12-05 18:49:27 +0530

    redesigned home-layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,188 +1,222 @@
-@extends('layouts.navscrolled')
+<!DOCTYPE html>
+<html>
+<head>
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta content="" name="keywords">
+  <meta content="" name="description">
 
-@section('content')
+  <meta name="csrf-token" content="{{ csrf_token() }}">
 
-  <!--==========================
-  Slider
-  ============================-->
-  <section id="intro">
-    <div class="intro-container">
-      <div id="introCarousel" class="carousel slide carousel-fade" data-ride="carousel">
+  <title>{{ config('app.name', 'Lasya') }}</title>
 
-        <ol class="carousel-indicators"></ol>
+  <!-- Loding Scripts -->
+  <style>
+  .no-js #loader
+  {
+    display: none;
+  }
+  .js #loader
+  {
+    display: block;
+    position: absolute;
+    left: 100px;
+    top: 0;
+  }
+  .se-pre-con
+  {
+    position: fixed;
+    left: 0px;
+    top: 0px;
+    width: 100%;
+    height: 100%;
+    z-index: 9999;
+    background: url(assets/img/loading/Preloader_7.gif) center no-repeat #fff;
+  }
+  </style>
 
-        <div class="carousel-inner" role="listbox">
+  <!-- Scripts -->
+  {{-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"> --}}
+  <link href="{{ asset('assets\lib\bootstrap\css\bootstrap.css') }}" rel="stylesheet" >
+  <link href="{{ asset('assets\css\styles.css') }}" rel="stylesheet">
+  <link href="{{ asset('assets\lib\font-awesome\css\font-awesome.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('assets\lib\animate\animate.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('assets\lib\ionicons\css\ionicons.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('assets\lib\owlcarousel\assets\owl.carousel.min.css') }}" rel="stylesheet">
 
-          <div class="carousel-item active">
-            <div class="carousel-background"><img src="assets/img/intro-carousel/1.jpg" alt=""></div>
-            <div class="carousel-container" style="justify-content: center; align-items: center;">
-              <div class="carousel-content">
-                <h2 class="p-5">Online learning is the future of education, and it is happening right now all around us.</h2>
-                <!--<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                <a href="#featured-services" class="btn-get-started scrollto">Get Started</a>-->
-              </div>
-            </div>
-          </div>
-          <a href="#title-main" class="mouse-hover scrollto"><div class="mouse"></div></a>
-        </div>
-      </div>
+</head>
+<body>
+  <div class="se-pre-con"></div>
+  <header id="header"
+  style="background: rgba(0, 0, 0, 0.9);
+  padding: 20px 0;
+  height: 72px;
+  transition: all 0.5s;">
+  <div class="container-fluid">
+    <div id="logo" class="pull-left">
+      <h1><a href="{{ url('/') }}" class="scrollto">
+        LASYA
+      </a></h1>
     </div>
+    <nav id="nav-menu-container">
+      <ul class="nav-menu">
+        @guest
+          <li class="menu-active">
+            <a href="{{ route('login') }}">{{ __('Login') }}</a>
+          </li>
+          @if(Route::has('register'))
+            <li><a href="{{ route('register') }}">{{ __('Register') }}</a></li>
+          @endif
+        @else
+          <li class="menu-has-children"><a href="">{{ Auth::user()->name }}</a>
+            <ul>
+              <li><a href="/home/profile/{{ Auth::user()->id }}/edit">Profile</a></li>
+              <li><a href="{{ route('logout') }}"
+                onclick="event.preventDefault();
+                document.getElementById('logout-form').submit();">
+                {{ __('Logout') }}</a>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                  @csrf
+                </form>
+              </li>
+            @endguest
+          </ul>
+        </li>
+      </ul>
+    </nav><!-- #nav-menu-container -->
+  </div>
+</header><!-- #header -->
 
-    <main id="main">
-      <!--==========================
-      About Us Section
-      ============================-->
-      <section id="about">
-        <div class="container">
-          <div class="d-flex align-items-center p-3 my-3 text-white-50 rounded shadow-sm wow fadeInUp" style="background-color: #6f42c1; box-shadow:5px 5px 10px;">
-            <img class="mr-3" src="assets/assets/img/intro-carousel/bootstrap-outline.svg" alt="" width="48" height="48">
-            <div class="lh-100">
-              <h6 class="mb-0 text-white lh-100" id="title-main">Bootstrap</h6>
-              <small>Since 2011</small>
+<body style="padding-top:6rem;">
+  <section style="min-height:500px;">
+    <div class="container-fluid">
+
+      <div class="row px-4">
+
+        <div class="col-md-3 px-3">
+          <div class="card">
+            <div class="card-header">
+              Featured
             </div>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item">Cras justo odio</li>
+              <li class="list-group-item">Dapibus ac facilisis in</li>
+              <li class="list-group-item">Vestibulum at eros</li>
+              <li class="list-group-item text-center">
+                <a href="/Account/Login" class="btn btn-info btn-block">Login/Register</a></td>
+              </li>
+            </ul>
           </div>
+        </div>
 
-          <div class="my-3 p-3 rounded shadow-sm">
-            <h6 class="border-bottom border-gray pb-2 mb-0 wow fadeInLeft" data-wow-delay="0.1s">Recent updates</h6>
-            <div class="media text-muted pt-3 wow fadeInLeft" data-wow-delay="0.1s">
-              <svg class="bd-placeholder-img mr-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 32x32"><title>Placeholder</title><rect width="100%" height="100%" fill="#007bff"/><text x="50%" y="50%" fill="#007bff" dy=".3em">32x32</text></svg>
-              <p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray text-dark">
-                <strong class="d-block">@username</strong>
-                Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
-              </p>
-            </div>
-            <div class="media text-muted pt-3 wow fadeInLeft" data-wow-delay="0.2s">
-              <svg class="bd-placeholder-img mr-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 32x32"><title>Placeholder</title><rect width="100%" height="100%" fill="#e83e8c"/><text x="50%" y="50%" fill="#e83e8c" dy=".3em">32x32</text></svg>
-              <p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray text-dark">
-                <strong class="d-block">@username</strong>
-                Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
-              </p>
-            </div>
-            <div class="media text-muted pt-3 wow fadeInLeft" data-wow-delay="0.3s">
-              <svg class="bd-placeholder-img mr-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 32x32"><title>Placeholder</title><rect width="100%" height="100%" fill="#6f42c1"/><text x="50%" y="50%" fill="#6f42c1" dy=".3em">32x32</text></svg>
-              <p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray text-dark">
-                <strong class="d-block">@username</strong>
-                Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
-              </p>
-            </div>
-            <small class="d-block text-right mt-3">
-              <a href="#">All updates</a>
-            </small>
-          </div>
+        <div class="col-md-9 p-md-0 p-3">
+          <div class="row">
 
-          <div class="my-3 p-3 rounded shadow-sm">
-            <h6 class="border-bottom border-gray pb-2 mb-0">Suggestions</h6>
-            <div class="media text-muted pt-3">
-              <svg class="bd-placeholder-img mr-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 32x32"><title>Placeholder</title><rect width="100%" height="100%" fill="#007bff"/><text x="50%" y="50%" fill="#007bff" dy=".3em">32x32</text></svg>
-              <div class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
-                <div class="d-flex justify-content-between align-items-center w-100">
-                  <strong class="text-gray-dark">Full Name</strong>
-                  <a href="#">Follow</a>
+            <div class="col-sm-4">
+              <div class="custom-card custom-card-1">
+                <div class="custom-card-body text-left">
+                  <h3>Card title</h3>
                 </div>
-                <span class="d-block">@username</span>
               </div>
             </div>
-            <div class="media text-muted pt-3">
-              <svg class="bd-placeholder-img mr-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 32x32"><title>Placeholder</title><rect width="100%" height="100%" fill="#007bff"/><text x="50%" y="50%" fill="#007bff" dy=".3em">32x32</text></svg>
-              <div class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
-                <div class="d-flex justify-content-between align-items-center w-100">
-                  <strong class="text-gray-dark">Full Name</strong>
-                  <a href="#">Follow</a>
+
+            <div class="col-sm-4 pt-md-0 pt-3">
+              <div class="custom-card custom-card-2">
+                <div class="custom-card-body text-left">
+                  <h3>Card title</h3>
                 </div>
-                <span class="d-block">@username</span>
               </div>
             </div>
-            <div class="media text-muted pt-3">
-              <svg class="bd-placeholder-img mr-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 32x32"><title>Placeholder</title><rect width="100%" height="100%" fill="#007bff"/><text x="50%" y="50%" fill="#007bff" dy=".3em">32x32</text></svg>
-              <div class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
-                <div class="d-flex justify-content-between align-items-center w-100">
-                  <strong class="text-gray-dark">Full Name</strong>
-                  <a href="#">Follow</a>
+
+            <div class="col-sm-4 pt-md-0 pt-3">
+              <div class="custom-card custom-card-3">
+                <div class="custom-card-body text-left">
+                  <h3>Card title</h3>
                 </div>
-                <span class="d-block">@username</span>
               </div>
             </div>
-            <small class="d-block text-right mt-3">
-              <a href="#">All suggestions</a>
-            </small>
-          </div>
-        </div>
-      </section>
 
-
-
-
-    </main>
-
-
-    <!--==========================
-    Contact US
-    ============================-->
-    <section id="contact" class="section-bg">
-      <div class="container">
-
-        <div class="section-header">
-          <h3>Contact Us</h3>
-          <p class="text-dark">Satisifed or not we need to know your ever feedback towards UGHUB ...<br> So fell free to contact us or mail upon</p>
-        </div>
-
-        <div class="row contact-info">
-
-          <div class="col-md-4">
-            <div class="contact-address">
-              <i class="ion-ios-location-outline"></i>
-              <h3>Address</h3>
-              <address>Bengaluru, Karnataka - 560061, INDIA</address>
-            </div>
-          </div>
-
-          <div class="col-md-4">
-            <div class="contact-phone">
-              <i class="ion-ios-telephone-outline"></i>
-              <h3>Phone Number</h3>
-              <p>+91 9945887177</p>
-            </div>
-          </div>
-
-          <div class="col-md-4">
-            <div class="contact-email">
-              <i class="ion-ios-email-outline"></i>
-              <h3>Email</h3>
-              <p><a href="mailto:support@ughub.in">support@ughub.in</a></p>
-            </div>
-          </div>
-
-        </div>
-
-        <div class="form">
-          <div id="sendmessage">Your message has been sent. Thank you!</div>
-          <div id="errormessage"></div>
-          <form action="" method="post" role="form" class="contactForm">
-            <div class="form-row">
-              <div class="form-group col-md-6">
-                <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
-                <div class="validation"></div>
-              </div>
-              <div class="form-group col-md-6">
-                <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
-                <div class="validation"></div>
+            <div class="col-sm-4 pt-3">
+              <div class="custom-card custom-card-3">
+                <div class="custom-card-body text-left">
+                  <h3>Card title</h3>
+                </div>
               </div>
             </div>
-            <div class="form-group">
-              <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
-              <div class="validation"></div>
-            </div>
-            <div class="form-group">
-              <textarea class="form-control" name="message" rows="5" data-rule="required" data-msg="Please write something for us" placeholder="Message"></textarea>
-              <div class="validation"></div>
-            </div>
-            <div class="text-center"><button type="submit">Send Message</button></div>
-          </form>
-        </div>
 
+            <div class="col-sm-4 pt-3">
+              <div class="custom-card custom-card-1">
+                <div class="custom-card-body text-left">
+                  <h3>Card title</h3>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-sm-4 pt-3">
+              <div class="custom-card custom-card-2">
+                <div class="custom-card-body text-left">
+                  <h3>Card title</h3>
+                </div>
+              </div>
+            </div>
+
+            </div>
+          </div>
+        </div>
       </div>
-    </section><!-- #contact -->
+</section>
+</body>
+<!--==========================
+Footer
+============================-->
+<footer id="footer" >
+  <div class="container">
+    <div class="copyright">
+      &copy; Copyright <strong>Lasya</strong>. All Rights Reserved <br>
+      <a href="privacypolicys.html">Privacy Policy</a>
+      <a class="pl-3" href="faqs.html">FAQ's</a>
 
-  </main>
+    </footer><!-- #footer -->
 
-@endsection
+
+    <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+
+
+    <!-- jquerys -->
+    <script src="{{ asset('assets/lib/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('assets/lib/jquery/jquery-migrate.min.js') }}"></script>
+
+    <!-- bootstrap -->
+    <script src="{{ asset('assets/lib/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+
+    <!-- for to the top animation -->
+    <script src="{{ asset('assets/lib/easing/easing.min.js') }}"></script>
+
+    <!-- superfish for slider bg adjust -->
+    <script src="{{ asset('assets/lib/superfish/hoverIntent.js') }}"></script>
+    <script src="{{ asset('assets/lib/superfish/superfish.min.js') }}"></script>
+
+    <!-- wow animation -->
+    <script src="{{ asset('assets/lib/wow/wow.min.js') }}"></script>
+
+    <!-- slider animation for sliding left or right ( swipe ) -->
+    <script src="{{ asset('assets/lib/waypoints/waypoints.min.js') }}"></script>
+    <script src="{{ asset('assets/lib/touchSwipe/jquery.touchSwipe.min.js') }}"></script>
+
+    <!-- javascript main -->
+    <script src="{{ asset('assets/js/main.js') }}"></script>
+
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js"></script>
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.2/modernizr.js"></script>
+
+    <!-- loading -->
+    <script>
+    $(window).load(function()
+    {
+      // Animate loader off screen
+      $(".se-pre-con").fadeOut("slow");;
+    });
+    </script>
+
+
+  </body>
+  </html>
